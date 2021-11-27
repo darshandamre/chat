59c1c8d45f10c03e36708f1a94b55eca613013f0
Darshan Damre <darshandamre5@gmail.com>
2021-11-28 00:02:52 +0530

    typeorm setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello there");
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { User } from "./entities/User";
+
+const main = async () => {
+  const conn = await createConnection({
+    type: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "postgres",
+    database: "chat",
+    logging: true,
+    synchronize: true,
+    entities: [User]
+  });
+};
+
+main().catch(err => {
+  console.log(err);
+});
